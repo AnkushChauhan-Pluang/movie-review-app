a4e1091ae6a92f2f45ffed453603567c3a5b615a
AnkushChauhan-Pluang <ankush.chauhan@pluang.com>
2022-02-07 19:37:48 +0530

    Added link to movie details page(dynamic url)

--- components/movie/MovieListItem.jsx
+++ components/movie/MovieListItem.jsx
@@ -1,19 +1,32 @@
-import Image from "next/image";
-import React from "react";
+import Link from '@components/common/Link'
+import Image from 'next/image'
+import React from 'react'
 
 const MovieListItem = ({ movie }) => {
+  // console.log(movie)
+  const urlTitle = movie.original_title
+    .replace(':', '')
+    .replaceAll(' ', '-')
+    .toLowerCase()
+
+  const href = `/movie/${urlTitle}?id=${movie.id}`
+
   return (
     <div className="">
       <div className="flex overflow-hidden rounded-md">
-        <Image
-          src={`https://www.themoviedb.org/t/p/original/${movie.poster_path}`}
-          height={300}
-          width={200}
-        />
+        <Link href={href}>
+          <Image
+            src={`https://www.themoviedb.org/t/p/original/${movie.poster_path}`}
+            height={300}
+            width={200}
+          />
+        </Link>
       </div>
-      <p className="max-w-[200px]">{movie.original_title}</p>
+      <Link href={href}>
+        <h2 className="max-w-[200px]">{movie.original_title}</h2>
+      </Link>
     </div>
-  );
-};
+  )
+}
 
-export default MovieListItem;
+export default MovieListItem
