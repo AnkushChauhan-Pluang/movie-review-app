057e35ad77ee5cd0af49cc7f5a64935b709e9277
AnkushChauhan-Pluang <ankush.chauhan@pluang.com>
2022-02-15 16:24:21 +0530

    Improved client side form validation

--- components/common/FormField.jsx
+++ components/common/FormField.jsx
@@ -1,12 +1,19 @@
-import { Field } from 'formik'
+import { ErrorMessage, Field } from 'formik';
 
 const FormField = (props) => {
   return (
-    <Field
-      {...props}
-      className="w-full rounded border p-2 text-sm placeholder:text-sm"
-    />
-  )
-}
+    <div>
+      <Field
+        {...props}
+        className="w-full rounded border p-2 text-sm placeholder:text-sm"
+      />
+      <ErrorMessage
+        name={props.name}
+        component="div"
+        className="mt-1 rounded bg-red-100 px-2 py-1 text-xs text-red-500"
+      />
+    </div>
+  );
+};
 
-export default FormField
+export default FormField;
--- utils/validationSchema.js
+++ utils/validationSchema.js
@@ -1,13 +1,21 @@
-import * as yup from 'yup'
+import * as yup from 'yup';
 
 export const LoginSchema = yup.object({
-  email: yup.string().email().required(),
-  password: yup.string().required(),
-})
+  email: yup.string().email('Invalid email').matches().required('Email is required'),
+  password: yup.string().required('Password is required'),
+});
 
 export const SignupSchema = yup.object({
-  email: yup.string().email().required(),
-  fullName: yup.string().required(),
-  username: yup.string().required(),
-  password: yup.string().required(),
-})
+  email: yup.string().email('Invalid email').required(),
+  username: yup.string().min(3).required('Username is required'),
+  password: yup.string().min(3).required('Password is required'),
+  confirmPassword: yup
+    .string()
+    .when('password', {
+      is: (val) => (val && val.length > 0 ? true : false),
+      then: yup
+        .string()
+        .oneOf([yup.ref('password')], 'Both password need to be the same'),
+    })
+    .required('Confirmation password is required'),
+});
