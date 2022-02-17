97c223842a5d801ae19056212e534bfae3d7d1bf
AnkushChauhan-Pluang <ankush.chauhan@pluang.com>
2022-02-17 22:03:49 +0530

    Bug fix: Redirect logged in user to home page

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,12 +1,19 @@
-import LoginView from '@components/auth/LoginView'
-import React from 'react'
+import LoginView from '@components/auth/LoginView';
+import { useAuthContext } from 'contexts/AuthContext';
+import { useRouter } from 'next/router';
+import React from 'react';
 
 const Login = () => {
+  const { loginState } = useAuthContext();
+  const router = useRouter();
+
+  if (loginState.user) router.replace('/');
+
   return (
-    <div className='my-8'>
+    <div className="my-8">
       <LoginView />
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- pages/signup.jsx
+++ pages/signup.jsx
@@ -1,12 +1,19 @@
-import SignupView from '@components/auth/SignupView'
-import React from 'react'
+import SignupView from '@components/auth/SignupView';
+import { useAuthContext } from 'contexts/AuthContext';
+import { useRouter } from 'next/router';
+import React from 'react';
 
 const Signup = () => {
+  const { loginState } = useAuthContext();
+  const router = useRouter();
+
+  if (loginState.user) router.replace('/');
+  
   return (
-    <div className='my-8'>
+    <div className="my-8">
       <SignupView />
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
