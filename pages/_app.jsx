e0408e67fdcddcfeab285abaf7f346630fb7c4dc
AnkushChauhan-Pluang <ankush.chauhan@pluang.com>
2022-02-22 16:35:04 +0530

    Setup Material-Ui

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,30 @@
 import Layout from '@components/common/Layout';
+import { CacheProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
 import { AuthContextProvider } from 'contexts/AuthContext';
+import createEmotionCache from 'src/createEmotionCache';
+import theme from 'src/theme';
 import '../styles/globals.css';
 
-const MyApp = ({ Component, pageProps }) => {
+const clientSideEmotionCache = createEmotionCache();
+
+const MyApp = ({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}) => {
   return (
-    <AuthContextProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </AuthContextProvider>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AuthContextProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </AuthContextProvider>
+      </ThemeProvider>
+    </CacheProvider>
   );
 };
 
